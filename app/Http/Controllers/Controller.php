f7314796c522b6fa71a0e68968209ffc6f8b8e64
unknown <batouljdid25@gmail.com>
2025-03-06 13:42:58 +0300

    add zone controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function failedAuthorization()
+    {
+        throw new AuthorizationException('You are not authorized to perform this action.');
+    }
 }
