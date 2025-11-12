67486858f1ab05b7d6cfc50836d41d09310c495e
ward_alshahaheen <wardalshaheen1@gmail.com>
2025-11-12 19:07:16 +0300

    edit UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,22 +10,7 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        $positions = ['Head', 'CoHead', 'Senior leader', 'Junior leader', 'Volunteer'];
-        $departments = ['IT & AI DEPARTMENT', 'RESEARCH DEPARTMENT', 'DESIGN DEPARTMENT', 'ADMIN DEPARTMENT', 'EDUCATION DEPARTMENT', 'MEDIA DEPARTMENT', 'FUNDRISING DEPARTMENT'];
-        $layers = ['public health', 'resources management', 'economic factor', 'urban planning', 'ecological factor', 'social factor', 'building code', 'Culture and heritage', 'technology and infrastructure', 'data collection and analysis'];
-
-        for ($i = 1; $i <= 10; $i++) {
-            User::create([
-                'name' => "User $i",
-                'email' => "user$i@example.com",
-                'password' => Hash::make('password123'),
-                'position' => $positions[array_rand($positions)],
-                'department' => $departments[array_rand($departments)],
-                'layer' => $layers[array_rand($layers)],
-            ]);
-        }
-
-         // إنشاء مستخدم خاص بقسم الـ ADMIN DEPARTMENT
+        // إنشاء مستخدم خاص بقسم الـ ADMIN DEPARTMENT
         User::create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
@@ -36,7 +21,7 @@ class UserSeeder extends Seeder
             'is_verified' => true
         ]);
     }
-    }
+}
 
 
 
